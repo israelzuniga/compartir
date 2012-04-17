b8b512cb3f9817c3878ecf853a5da84904e42f4e
Marcos Mellado <marcos@ooyala.com>
2012-04-17 00:37:30 -0700

    adding basic styles

--- app.js
+++ app.js
@@ -32,5 +32,16 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
+app.get('/new', function(req, res) {
+  var user = {matricula:"d242543645", name:"Marcos Mellado"}
+  res.render('newride.jade', {
+    locals: { 
+    	user: user,
+    	title: 'Crear nuevo viaje',
+    	classes: "new-ride"
+    }
+  });
+});
+
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
