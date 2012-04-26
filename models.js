7a3f1794ab743b8698e333ee06e5aad39042e330
Israel Zuñiga <israel@HAL9000.(none)>
2012-04-26 03:40:54 -0500

    DB

---
+++ models.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose')
+	, Schema = mongoose.Schema
+	, ObjectId = Schema.ObjectId;
+	
+var rideSchema = new Schema({
+	nride: ObjectId,
+	type_u: String,
+	origin: String,
+	destination: String,
+	schedule_departure_time: String,
+	schedule_return_time: String, 
+	comments: String
+});
+
+module.exports = mongoose.model('Ride', rideSchema);
